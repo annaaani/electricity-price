cd86932abea0efab120e9ce9fde4a230a0efc2e6
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-05 10:51:04 +0200

    LocalSwitcher radio buttons working

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,48 @@
 <script lang="ts">
-    import ElectricityData from "./ElectricityData.svelte";
+    import LocalSwitcher from "./components/LocalSwitcher.svelte";
+    import DateComponent from "./components/DateComponent.svelte";
+
+    interface PriceData {
+        timestamp: number;
+        price: number;
+    }
+
+    let local: "lv" | "ee" | "fi" | "lt" = "ee";
+    let data: Record<string, PriceData[]> | null = null;
+    let error: String | null = null;
+
+    $: {
+        data = null;
+        error = null;
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://dashboard.elering.ee/api/nps/price?start=2024-11-03T22:00:00.000Z&end=2024-11-04T21:59:59.999Z');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const jsonData = await response.json();
+                data = jsonData.data[local];
+            } catch (err) {
+                error = err.message;
+            }
+        };
+
+        fetchData();
+    }
 </script>
 
 <main>
-    <h1>Electricity prices</h1>
-    <ElectricityData/>
+    {#if error}
+        <p>Error: {error}</p>
+    {:else if data}
+        <h1>Electricity prices</h1>
+        <DateComponent/>
+        <LocalSwitcher bind:local={local}/>
+        <!--        <Histogram {}>-->
+
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+    {:else}
+        <p>Loading...</p>
+    {/if}
 </main>
-
-<style>
-    .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-        transition: filter 300ms;
-    }
-
-    .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-
-    .read-the-docs {
-        color: #888;
-    }
-</style>
---
+++ src/components/LocalSwitcher.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let countries: String[] =  ["ee","lv", "lt", "fi"];
+    export let local: string = countries[0];
+</script>
+
+{#each countries as country}
+    <label>
+        <input
+                type="radio"
+                name="countryOption"
+                value={country}
+                bind:group={local}
+        />
+        {country}
+    </label>
+{/each}
+
+<style>
+
+</style>
