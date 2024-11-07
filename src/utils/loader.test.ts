9ed33641622b210f7320b3944781eec01f9091e1
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-07 11:42:36 +0200

    loader summertime test

--- src/utils/loader.test.ts
+++ src/utils/loader.test.ts
@@ -2,9 +2,12 @@ import {describe, expect, test, vi} from "vitest"
 import {fetchData, fetchUrl} from "./loader"
 
 describe('loader.ts', () => {
-    test('fetches full day of prices with correct UTC timestamps', () => {
+    test('fetches full day of prices with correct UTC timestamps - wintertime', () => {
         expect(fetchUrl('2024-11-06')).to.eq('https://dashboard.elering.ee/api/nps/price?start=2024-11-05T22:00:00.000Z&end=2024-11-06T21:59:00.000Z')
-        // TODO: test summer time
+    })
+
+    test('fetches full day of prices with correct UTC timestamps - summertime ', () => {
+        expect(fetchUrl('2024-06-06')).to.eq('https://dashboard.elering.ee/api/nps/price?start=2024-06-05T21:00:00.000Z&end=2024-06-06T20:59:00.000Z')
     })
 
     test('fetches Elering price data and returns json data', async () => {
@@ -24,10 +27,7 @@ describe('loader.ts', () => {
 
     test('throws an error when fetch fails', async () => {
         // @ts-ignore
-        // TODO
-        (fetch as vi.Mock).mockResolvedValueOnce({
-            ok: false,
-        })
+        vi.spyOn(window, 'fetch').mockResolvedValue({ok: false, json: async () => mockResponse} as Response)
 
         const date = '2024-11-06';
         await expect(fetchData(date)).rejects.toThrow('Network response was not ok');
