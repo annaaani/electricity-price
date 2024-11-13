e99a6ac62b085959a88a6adc670530597cd9b98d
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-13 09:51:06 +0200

    default language fallback in case local storage lang does not exist in list

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,9 @@
 import type en from './en.json'
 import langs from './langs.json'
 
-const navLanguage = navigator.language.split("-")[0] // todo: WHAT IF i AM COMING FROM jAPAN?
-
-console.log(navLanguage)
-export const lang = localStorage['lang'] ?? navLanguage ?? langs[0]
+const navLanguage = navigator.language.split("-")[0]
+const localStorageLang = localStorage['lang'] in langs ? localStorage['lang'] : null
+export const lang = localStorageLang ?? navLanguage ?? langs[0]
 
 export function changeLang(lang: typeof langs[number]) {
     localStorage['lang'] = lang
