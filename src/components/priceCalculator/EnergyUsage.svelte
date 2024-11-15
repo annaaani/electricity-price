f01a48a0634638d0e4b4d489388bc0b224ed50be
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-15 14:50:36 +0200

    duration is bindale using effect

--- src/components/priceCalculator/EnergyUsage.svelte
+++ src/components/priceCalculator/EnergyUsage.svelte
@@ -1,16 +1,37 @@
-<script>
-    import UsageDuration from './Time.svelte'
+<script lang="ts">
+    interface Device {
+        watts: number
+        hours: number
+    }
+
+    import Time from './Time.svelte'
     import DeviceSwitcher from './DeviceSwitcher.svelte'
     import DisplayPrice from './DisplayPrice.svelte'
     import CalcStartOption from "./CalcStartOption.svelte";
+    import devicesData from '../../assets/devices.json'
 
-    let device = $state()
+    let timeDevice: string | undefined = $state();
+
+    let d: Device | undefined;
+    let duration = $state()
+
+    $effect(() => {
+        d = (Object(devicesData)[timeDevice!]) as Device;
+        if (d) {
+            duration = d.hours;
+        }
+    })
 </script>
 
-
-<UsageDuration/>
-<DeviceSwitcher bind:device/>
+<div class="space"></div>
+<DeviceSwitcher bind:timeDevice/>
+<Time bind:duration/>
 <CalcStartOption/>
 <DisplayPrice/>
 
 
+<style>
+    .space {
+        padding: 2em 0;
+    }
+</style>
