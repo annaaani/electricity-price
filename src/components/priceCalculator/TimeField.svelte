55b8439c7f59a6bcd2e906c1f918504759a3bfca
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-18 13:36:03 +0200

    add max duration according to the time of the day and available hours

--- src/components/priceCalculator/TimeField.svelte
+++ src/components/priceCalculator/TimeField.svelte
@@ -2,10 +2,9 @@
 
     import {t} from '../../i18n'
     let {duration = $bindable()} = $props()
-    // TODO: add max duration according to the time of the day and available hours
 </script>
 
 <label class="input-container duration">
-    <input type="number" bind:value={duration} min="0"/>
+    <input type="number" bind:value={duration} min="0" max={String(23 - new Date().getHours())}/>
     <span class="unit">{t.hour}</span>
 </label>
