cfef5c79d608d230ce13a2efca0e55348e7df0a0
Anna, Liis U <ani.annabrita@gmail.com>
2024-11-19 10:11:34 +0200

    mock and test prices

--- src/utils/calcElectricityPrice.test.ts
+++ src/utils/calcElectricityPrice.test.ts
@@ -1,10 +1,17 @@
-import {describe, expect, test} from "vitest"
+import {describe, expect, test, vi} from "vitest"
 import {calcElectricityPrice} from "./calcElectricityPrice";
 
 describe('calcElectricityPrice.ts', () => {
+
     test('calculates correct price for device usage', () => {
-        // TODO:  before current price sum is 0
-        expect(calcElectricityPrice(2, 800, 3,[10.6, 11.2, 9, 17, 16.4, 19, 10.4, 12.2, 9.1, 14, 18.4, 24, 21.4, 20.5, 10.5, 11.9, 9.8, 15, 16.1, 18, 10.9, 12.4, 9.6, 17]))
-            .to.eq('0.273')
+        vi.spyOn(Date.prototype, 'getHours').mockReturnValue(1);
+        expect(calcElectricityPrice(2, 800, 3, [10.6, 11.2, 9, 17, 16.4, 19, 10.4, 12.2, 9.1, 14, 18.4, 24, 21.4, 20.5, 10.5, 11.9, 9.8, 15, 16.1, 18, 10.9, 12.4, 9.6, 17]))
+            .to.eq('0.267')
+    })
+
+    test('return 0 if time is before current hour', () => {
+        vi.spyOn(Date.prototype, 'getHours').mockReturnValue(4);
+        expect(calcElectricityPrice(2, 800, 3, [10.6, 11.2, 9, 17, 16.4, 19, 10.4, 12.2, 9.1, 14, 18.4, 24, 21.4, 20.5, 10.5, 11.9, 9.8, 15, 16.1, 18, 10.9, 12.4, 9.6, 17]))
+            .to.eq(0)
     })
 })
--- src/utils/calcElectricityPrice.ts
+++ src/utils/calcElectricityPrice.ts
@@ -1,11 +1,12 @@
 export function calcElectricityPrice(duration: number, watts: number, startTime: number, hourlyPrices: any) {
     let total = 0;
-    if (!hourlyPrices || hourlyPrices.length <= 1 || duration <= 0 || startTime < new Date().getHours() + 1 || watts <= 0) {
+    let currentHour = new Date().getHours() + 1
+    if (!hourlyPrices || hourlyPrices.length <= 1 || duration <= 0 || startTime < currentHour || watts <= 0) {
         return 0;
     }
 
-    for (let i = startTime; i < startTime+duration; i++) {
-        total += ((watts / 1000) * hourlyPrices[i])/100
+    for (let i = startTime; i < startTime + duration; i++) {
+        total += ((watts / 1000) * hourlyPrices[i]) / 100
     }
     return total.toFixed(3);
 }
